f6f00afe5034caeaffdbec4b9f72c1ef70aabbc1
Dennis <dennis.kuznik@novaskystories.com>
2025-12-08 01:11:18 +0100

    Move implementation to recursive_shared_mutex_impl,

--- src/recursive_shared_mutex.cpp
+++ src/recursive_shared_mutex.cpp
@@ -5,51 +5,134 @@
 
 namespace {
 
-class LocalLocks {
+class recursive_shared_mutex_impl {
 public:
+    static recursive_shared_mutex_impl& Instance();
+
+    void lock( std::shared_mutex& mutex ) noexcept;
+    [[nodiscard]] bool try_lock( std::shared_mutex& mutex ) noexcept;
+    void unlock( std::shared_mutex& mutex ) noexcept;
+    
+    void lock_shared( std::shared_mutex& mutex ) noexcept;
+    [[nodiscard]] bool try_lock_shared( std::shared_mutex& mutex ) noexcept;
+
+private:
     using Counter = std::make_signed_t<size_t>;
     using Entry   = std::pair<const void*,Counter>;
     using List    = std::vector<Entry>;
     using Iter    = List::iterator;
 
-    static LocalLocks& Instance();
+    recursive_shared_mutex_impl() = default;
+    ~recursive_shared_mutex_impl() noexcept;
+    recursive_shared_mutex_impl(const recursive_shared_mutex_impl&)            = delete;
+    recursive_shared_mutex_impl& operator=(const recursive_shared_mutex_impl&) = delete;
 
-    Iter get( const phreak_mutex::recursive_shared_mutex* instance );
-    void erase( Iter iterator );
-private:
-    LocalLocks() = default;
-    ~LocalLocks() noexcept;
-    LocalLocks(const LocalLocks&)            = delete;
-    LocalLocks& operator=(const LocalLocks&) = delete;
-private:
+    Iter make( const std::shared_mutex& mutex );
+
     List mList;
 };
 
-LocalLocks& LocalLocks::Instance()
+recursive_shared_mutex_impl& recursive_shared_mutex_impl::Instance()
 {
-    thread_local static LocalLocks res{};
+    thread_local static recursive_shared_mutex_impl res{};
 
     return res;
 }
 
-LocalLocks::Iter LocalLocks::get( const phreak_mutex::recursive_shared_mutex* instance )
+void recursive_shared_mutex_impl::lock( std::shared_mutex& mutex ) noexcept
 {
-    if ( auto iterator{
-        std::find_if( mList.rbegin(), mList.rend(),
-        [&] ( const Entry& entry ) { return entry.first == instance; } ) };
-        iterator != mList.rend() )
-    {
-        return ( ++iterator ).base();
+    auto it{ make( mutex ) };
+
+    if ( it->second < 0 ) {
+        --it->second;
+    } else {
+        if ( it->second > 0 ) {
+            mutex.unlock_shared();
+        }
+        mutex.lock();
+        it->second = -( it->second + 1 );
     }
-    return mList.emplace( mList.end(), instance, 0 ); 
 }
 
-void LocalLocks::erase( Iter iterator )
+bool recursive_shared_mutex_impl::try_lock( std::shared_mutex& mutex ) noexcept
 {
-    mList.erase( std::move( iterator ) );
+    auto it{ make( mutex ) };
+
+    if ( it->second < 0 ) {
+        --it->second;
+        return true;
+    } else if ( it->second == 0 ) {
+        if ( mutex.try_lock() ) {
+            it->second = -1;
+            return true;
+        }
+        mList.erase( std::move( it ) );
+    }
+    return false;
 }
 
-LocalLocks::~LocalLocks() noexcept
+void recursive_shared_mutex_impl::unlock( std::shared_mutex& mutex ) noexcept
+{
+    auto it{ make( mutex ) };
+
+    assert( it->second != 0 );
+    if ( it->second > 0 ) {
+        if ( --it->second == 0 ) {
+            mutex.unlock_shared();
+            mList.erase( std::move( it ) );
+        }
+    } else if ( it->second < 0 ) {
+        if ( ++it->second == 0 ) {
+            mutex.unlock();
+            mList.erase( std::move( it ) );
+        }
+    } else {
+        std::terminate();
+    }
+}
+
+void recursive_shared_mutex_impl::lock_shared( std::shared_mutex& mutex ) noexcept
+{
+    auto it{ make( mutex ) };
+
+    if ( it->second < 0 ) {
+        --it->second;
+    } else {
+        if ( it->second == 0 ) {
+            mutex.lock_shared();
+        }
+        ++it->second;
+    }
+}
+
+bool recursive_shared_mutex_impl::try_lock_shared( std::shared_mutex& mutex ) noexcept
+{
+    auto it{ make( mutex ) };
+
+    if ( it->second < 0 ) {
+        --it->second;
+        return true;
+    } else if ( it->second == 0 && !mutex.try_lock_shared() ) {
+        mList.erase( std::move( it ) );
+        return false;
+    }
+    ++it->second;
+    return true;
+}
+
+recursive_shared_mutex_impl::Iter recursive_shared_mutex_impl::make( const std::shared_mutex& mutex )
+{
+    if ( auto rit{
+        std::find_if( mList.rbegin(), mList.rend(),
+        [&] ( const Entry& entry ) { return entry.first == &mutex; } ) };
+        rit != mList.rend() )
+    {
+        return ( ++rit ).base();
+    }
+    return mList.emplace( mList.end(), &mutex, 0 ); 
+}
+
+recursive_shared_mutex_impl::~recursive_shared_mutex_impl() noexcept
 {
     assert( mList.empty() );
 }
@@ -60,86 +143,27 @@ namespace phreak_mutex {
 
 void recursive_shared_mutex::lock() noexcept
 {
-    auto it{ LocalLocks::Instance().get( this ) };
-
-    if ( it->second < 0 ) {
-        --it->second;
-    } else {
-        if ( it->second > 0 ) {
-            mMutex.unlock_shared();
-        }
-        mMutex.lock();
-        it->second = -( it->second + 1 );
-    }
+    recursive_shared_mutex_impl::Instance().lock( mMutex );
 }
 
 bool recursive_shared_mutex::try_lock() noexcept
 {
-    auto& locks{ LocalLocks::Instance() };
-    auto it{ locks.get( this ) };
-
-    if ( it->second < 0 ) {
-        --it->second;
-        return true;
-    } else if ( it->second == 0 ) {
-        if ( mMutex.try_lock() ) {
-            it->second = -1;
-            return true;
-        }
-        locks.erase( std::move( it ) );
-    }
-    return false;
+    return recursive_shared_mutex_impl::Instance().try_lock( mMutex );
 }
 
 void recursive_shared_mutex::unlock() noexcept
 {
-    auto& locks{ LocalLocks::Instance() };
-    auto it{ locks.get( this ) };
-
-    assert( it->second != 0 );
-    if ( it->second > 0 ) {
-        if ( --it->second == 0 ) {
-            mMutex.unlock_shared();
-            locks.erase( std::move( it ) );
-        }
-    } else if ( it->second < 0 ) {
-        if ( ++it->second == 0 ) {
-            mMutex.unlock();
-            locks.erase( std::move( it ) );
-        }
-    } else {
-        std::terminate();
-    }
+    recursive_shared_mutex_impl::Instance().unlock( mMutex );
 }
 
 void recursive_shared_mutex::lock_shared() noexcept
 {
-    auto it{ LocalLocks::Instance().get( this ) };
-
-    if ( it->second < 0 ) {
-        --it->second;
-    } else {
-        if ( it->second == 0 ) {
-            mMutex.lock_shared();
-        }
-        ++it->second;
-    }
+    recursive_shared_mutex_impl::Instance().lock_shared( mMutex );
 }
 
 bool recursive_shared_mutex::try_lock_shared() noexcept
 {
-    auto& locks{ LocalLocks::Instance() };
-    auto it{ locks.get( this ) };
-
-    if ( it->second < 0 ) {
-        --it->second;
-        return true;
-    } else if ( it->second == 0 && !mMutex.try_lock_shared() ) {
-        locks.erase( std::move( it ) );
-        return false;
-    }
-    ++it->second;
-    return true;
+    return recursive_shared_mutex_impl::Instance().try_lock_shared( mMutex );
 }
 
 } // phreak_mutex
